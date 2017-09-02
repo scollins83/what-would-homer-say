41785493c9db533dff9ecec62c6e3278c1ba6242
Sara Collins <datadivallc@gmail.com>
2017-09-01 19:04:50 -0500

    Adding initial model files to git.

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, request
-import tensorflow as tf
-import sys
-import os
-
-sys.path.append(os.path.abspath("./model"))
+from flask import Flask, render_template
+import model
 
 app = Flask(__name__)
-global model, graph
-model, graph = init()
 
-@app.route
+
+@app.route("/")
+def home():
+    homer_utterance = model.infer()
+    return render_template("index.html", homer_utterance=homer_utterance)
+
+if __name__ == "__main__":
+    app.run(debug=True)
